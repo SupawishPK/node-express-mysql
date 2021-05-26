c4617edbb8b131e1b84cc032ac528d0da40f1f6f
supawish34 <supawish.kj@gmail.com>
2021-05-26 11:07:06 +0700

    test

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-const userRouter = require("./router/user.router");
+const dotenv = require("dotenv");
 
 app.use(express.json());
+dotenv.config();
 
+//router api
+const userRouter = require("./router/user.router");
+
+//call api
 app.use("/api", userRouter);
+
+//if we are here then the specified request is not found
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+//all other requests are not implemented.
+app.use((err, req, res, next) => {
+  res.status(err.status || 501);
+  res.json({
+    error: {
+      code: err.status || 501,
+      message: err.message,
+    },
+  });
+});
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server is running on port ${process.env.APP_PORT}! `);
